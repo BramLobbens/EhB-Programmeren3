2546b98a48c4ecd2ce11c3af112e93540a34bf10
BramLobbens <lobbensbram@gmail.com>
2019-10-18 20:09:43 +0200

    add: opdracht3

---
+++ Taken/Opdracht3/Oefening1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Oefening1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Oefening1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("905ad233-5f28-497d-9aaf-f82a373e1f94")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Taken/Opdracht3/Oefening1/Stapel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening1
+{
+    public class Stapel<T>
+    {
+        internal List<T> Values { get; set; }
+
+        public Stapel()
+        {
+            Values = new List<T>();
+        }
+
+        public void Toevoegen(T t)
+        {
+            Values.Add(t);
+        }
+
+        //Pop the first item from the list and return its value.
+        public T Verwijderen()
+        {
+            if (Values.Count > 0)
+            {
+                var first = Values.ElementAt(0);
+                Values.RemoveAt(0);
+
+                return first;
+            }
+            return default;
+        }
+
+        //Empty the list
+        public void Leegmaken()
+        {
+            Values.Clear();
+        }
+
+        public bool IsAanwezig(T t)
+        {
+            return Values.Contains(t);
+        }
+
+        //Make a Shallow copy of Values
+        public Stapel<T> Copy()
+        {
+            List<T> copy = Values.ConvertAll(value => value);
+            Stapel<T> newStapel = new Stapel<T>();
+            foreach(var val in copy)
+            {
+                newStapel.Toevoegen(val);
+            }
+            return newStapel;
+        }
+
+        public override string ToString()
+        {
+            return Values.ToString();
+        }
+
+    }
+}
