6469cc10f7f291ba935bdac369405c3f1cf64fae
BramLobbens <lobbensbram@gmail.com>
2019-11-16 15:08:45 +0100

    add: implement drawrectangles and colour selection

--- Taken/Opdracht4/Oefening3/Oefening3/Form1.Designer.cs
+++ Taken/Opdracht4/Oefening3/Oefening3/Form1.Designer.cs
@@ -28,19 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.addButton = new System.Windows.Forms.Button();
             this.removeButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.clearButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.contextMenuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(28, 26);
+            this.addButton.Location = new System.Drawing.Point(12, 32);
             this.addButton.Name = "addButton";
             this.addButton.Size = new System.Drawing.Size(75, 23);
             this.addButton.TabIndex = 0;
@@ -50,7 +55,7 @@
             // 
             // removeButton
             // 
-            this.removeButton.Location = new System.Drawing.Point(125, 26);
+            this.removeButton.Location = new System.Drawing.Point(110, 32);
             this.removeButton.Name = "removeButton";
             this.removeButton.Size = new System.Drawing.Size(75, 23);
             this.removeButton.TabIndex = 1;
@@ -58,42 +63,9 @@
             this.removeButton.UseVisualStyleBackColor = true;
             this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(800, 419);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.WordWrap = false;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 419);
-            this.panel1.TabIndex = 3;
-            // 
-            // panel2
-            // 
-            this.panel2.AutoSize = true;
-            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.panel2.Controls.Add(this.clearButton);
-            this.panel2.Controls.Add(this.addButton);
-            this.panel2.Controls.Add(this.removeButton);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 419);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 64);
-            this.panel2.TabIndex = 4;
-            // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(223, 26);
+            this.clearButton.Location = new System.Drawing.Point(200, 32);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(75, 23);
             this.clearButton.TabIndex = 2;
@@ -101,18 +73,62 @@
             this.clearButton.UseVisualStyleBackColor = true;
             this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(186, 26);
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.testToolStripMenuItem.Text = "Select Colour";
+            this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 508);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.clearButton);
+            this.panel1.Controls.Add(this.removeButton);
+            this.panel1.Controls.Add(this.addButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 428);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 80);
+            this.panel1.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 483);
-            this.Controls.Add(this.panel2);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.ContextMenuStrip = this.contextMenuStrip1;
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
             this.Name = "Form1";
             this.Text = "Oefening3";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,10 +138,13 @@
 
         private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.Button removeButton;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- Taken/Opdracht4/Oefening3/Oefening3/Form1.cs
+++ Taken/Opdracht4/Oefening3/Oefening3/Form1.cs
@@ -12,69 +12,134 @@ namespace Oefening3
 {
     public partial class Form1 : Form
     {
-        public List<string> symbols;
-        private const string symbol = " # ";
+        public List<Rectangle> _rectangles = new List<Rectangle>();
+        private int _numRectangles = 0; // Keep count of number of rectangles to draw
+        private int LocationX { get; set; } // default = 0
+        private int LocationY { get; set; } // default = 0
+        private Point RelativePoint { get; set; }
+        private Color RectangleColor { get; set; }
         public Form1()
         {
             InitializeComponent();
-            symbols = new List<string>();
+            UpdateToolStripStatus();
+            this.Paint += Form1_Paint;
         }
 
-        private void Draw()
+        private void addButton_Click(object sender, EventArgs e)
         {
-            textBox1.Clear();
-            // De dimensie van de vierkante matrix is een functie van een bovenafronding van de vierkantswortel van het aantal elementen.
-            // 1 -> 1
-            // 2 -> 2
-            // ...
-            // 4 -> 2
-            // 5 -> 3
-            // ...
-            // 9 -> 3
-            // 10 -> 4
-            // ...
-            // 16 -> 4
-            // etc.
-            var n = Math.Ceiling(Math.Sqrt(symbols.Count));
+            // Increment number of rectangles to draw
+            _numRectangles += 1;
+            
+            // Populate list of rectangles
+            PopulateRectangles();
+        }
 
-            var num = 1;
-            var count = symbols.Count;
-            var sb = new StringBuilder();
+        private void PopulateRectangles()
+        {
+            // Reset all rectangles
+            _rectangles.Clear();
+
+            var n = Math.Ceiling(Math.Sqrt(_numRectangles));
+            var count = _numRectangles;
+
+            var _rectangleHeight = (int) (this.Height / n);
+            var _rectangleWidth = (int) (this.Width / n);
+
             for (var row = 0; row < n; row++)
             {
                 for (var col = 0; col < n; col++)
                 {
                     if (count > 0)
                     {
-                        sb.Append($"  {num++:0000}  ");
+                        // Add rectangle to list
+                        var rectangle = new Rectangle(LocationX, LocationY, _rectangleWidth, _rectangleHeight); 
+                        this._rectangles.Add(rectangle);
+
                         count--;
                     }
+                    LocationX += _rectangleWidth;
                 }
-                sb.Append(Environment.NewLine);
+                LocationY += _rectangleHeight;
+                LocationX = 0; // Reset X offset
             }
+            LocationY = 0; // Reset Y offset
 
-            textBox1.Text = sb.ToString();
-        }
-
-        private void addButton_Click(object sender, EventArgs e)
-        {
-            symbols.Add(symbol);
-            Draw();
+            // Force Redraw the form
+            this.Invalidate();
         }
 
         private void removeButton_Click(object sender, EventArgs e)
         {
-            if (symbols.Count > 0)
+            if (_rectangles.Count > 0)
             {
-                symbols.RemoveAt(symbols.Count - 1);
+                _rectangles.RemoveAt(_rectangles.Count - 1); ;
+                _numRectangles -= 1;
             }
-            Draw();
+
+            // Force redraw the form
+            this.Invalidate(); 
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-            symbols.Clear();
-            Draw();
+            // Clear rectangles
+             _rectangles.Clear();
+            _numRectangles = 0;
+
+            // Reset mouse coordinates
+            RelativePoint = new Point(0, 0);
+            UpdateToolStripStatus();
+
+            // Force redraw the form
+            this.Invalidate(); 
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var rectangle in this._rectangles)
+            {
+                // Fill rectangle with colour if a colour is selected from the context menu and rectangle is selected
+                if (rectangle.Contains(RelativePoint) && RelativePoint.X != 0 && RelativePoint.Y != 0 && !RectangleColor.IsEmpty)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(RectangleColor), rectangle);
+                }
+                // Draw a black-outlined rectangle
+                else
+                {
+                    e.Graphics.DrawRectangle(Pens.Black, rectangle);
+                }
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            // Save the mouse pointer coordinates relative to the form
+            RelativePoint = this.PointToClient(Cursor.Position);
+
+            // Display the coordinates in the toolstrip
+            UpdateToolStripStatus();
+
+            // Force redraw the form
+            this.Invalidate();
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var form = new ColorDialog())
+            {
+                DialogResult dialogResult = form.ShowDialog();
+                if (dialogResult == DialogResult.OK)
+                {
+                    RectangleColor = form.Color;
+                    UpdateToolStripStatus();
+                }
+            }
+                
+        }
+
+        private void UpdateToolStripStatus()
+        {
+            toolStripStatusLabel1.Text = $"X: {RelativePoint.X}, Y: {RelativePoint.Y} | Color: {RectangleColor.Name}";
         }
     }
 }
